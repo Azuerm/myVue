7cc0a357acd45a33834e66303e018bb7ed89578f
Azuerm <3281186790@qq.com>
2025-04-01 15:39:38 +0800

    update: v-model案例

---
+++ 8 进阶语法/vue-model/src/components/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" :value="value" @input="handleChange">
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        value: String
+    },
+    methods: {
+        handleChange (e) {
+            this.$emit('input', e.target.value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+div {
+    margin: 30px;
+    width: 200px;
+    height: 30px;
+    background-color: pink;
+}
+</style>
