093b041e9e104e8e802fab96f310ec1c9fd75202
Azuerm <3281186790@qq.com>
2025-04-02 00:49:13 +0800

    update:插槽

---
+++ 10 插槽/vue-demo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+ <div>
+    <!-- 2.在使用组件时，在组件标签内填入内容 -->
+    <MyDialog>
+        <div>你确定要删除吗</div>
+    </MyDialog>
+    <MyDialog>你确认要退出吗</MyDialog>
+ </div>
+</template>
+ 
+<script>
+import MyDialog from './components/MyDialog.vue'
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {},
+  components: {
+    MyDialog
+  }
+}
+</script>
+ 
+<style scoped >
+body {
+    background-color: #b3b3b3;
+}
+</style>
